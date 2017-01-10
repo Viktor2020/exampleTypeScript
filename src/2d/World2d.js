50cfd579257ddd616d180d1c41c025ae188e2d6a
Viktor <viktorkostraba@gmail.com>
2017-01-11 01:16:53 +0300

    use ParticleContainer

--- src/2d/World2d.js
+++ src/2d/World2d.js
@@ -1,13 +1,17 @@
 define(["require", "exports", "../AppEvent", "./EventName2d", "../utill/Keyboard"], function (require, exports, AppEvent_1, EventName2d_1, Keyboard_1) {
     "use strict";
     var Sprite = PIXI.Sprite;
-    var TextureCache = PIXI.utils.TextureCache;
-    var Container = PIXI.Container;
-    var Rectangle = PIXI.Rectangle;
+    var ParticleContainer = PIXI.particles.ParticleContainer;
     var resources = PIXI.loader.resources;
     var World2d = (function () {
         function World2d() {
-            this.content = new Container();
+            this.content = new ParticleContainer(15000, {
+                position: true,
+                rotation: true,
+                alpha: false,
+                scale: false,
+                uvs: false
+            });
             this.sobEvent = new AppEvent_1.AppEvent(); // события
             this.nap = false;
             this.dimention = 1;
@@ -22,8 +26,8 @@ define(["require", "exports", "../AppEvent", "./EventName2d", "../utill/Keyboard
         };
         World2d.prototype.update = function () {
             this.cat.rotation += 0.01;
-            this.cat.x += this.cat.vx;
-            this.cat.y += this.cat.vy;
+            this.cat.x += this.cat.vx || 0;
+            this.cat.y += this.cat.vy || 0;
             if (this.rocket.x > 300) {
                 this.dimention = -1;
                 this.rocket.rotation = Math.PI;
@@ -35,26 +39,26 @@ define(["require", "exports", "../AppEvent", "./EventName2d", "../utill/Keyboard
             this.rocket.x += this.dimention;
         };
         World2d.prototype.testcreate = function () {
+            var id = resources["resource/images/sprites.json"].textures;
             //Create the `tileset` sprite from the texture
-            var texture = TextureCache["resource/images/tileset.png"];
-            //Create a rectangle object that defines the position and
-            //size of the sub-image you want to extract from the texture
-            var rectangle = new Rectangle(3 * 32, 2 * 32, 32, 32);
-            //Tell the texture to use that rectangular section
-            // texture.frame = rectangle; //Create the sprite from the texture
-            texture.frame = rectangle;
-            this.rocket = new Sprite(texture);
+            // var texture = TextureCache["resource/images/tileset.png"];
+            // //Create a rectangle object that defines the position and
+            // //size of the sub-image you want to extract from the texture
+            // var rectangle = new Rectangle(3 * 32, 2 * 32, 32, 32);
+            // //Tell the texture to use that rectangular section
+            // // texture.frame = rectangle; //Create the sprite from the texture
+            // texture.frame = rectangle;
+            this.rocket = new Sprite(id['6.png']);
             //Position the rocket sprite on the canvas
             this.rocket.position.set(100, 100);
             this.rocket.anchor.set(.5, .5);
             this.content.addChild(this.rocket);
             //Create the `cat` sprite from the texture
-            this.cat = new Sprite(resources["resource/images/imgif.gif"].texture);
+            this.cat = new Sprite(id['5.png']);
             this.cat.position.set(50, 50);
             this.cat.scale.set(0.5, 0.5);
             this.cat.anchor.set(0.5, 0.5);
             this.content.addChild(this.cat);
-            var id = resources["resource/images/sprites.json"].textures;
             console.log(resources, id);
             var sprite = new Sprite(id['1.png']);
             this.content.addChild(sprite);
--- src/2d/World2d.ts
+++ src/2d/World2d.ts
@@ -3,14 +3,21 @@ import {EventName2d} from "./EventName2d";
 import Sprite = PIXI.Sprite;
 import TextureCache = PIXI.utils.TextureCache;
 import Container = PIXI.Container;
+import ParticleContainer = PIXI.particles.ParticleContainer;
 import Rectangle = PIXI.Rectangle;
 import {keyboard} from "../utill/Keyboard";
-var resources = PIXI.loader.resources;
+let resources = PIXI.loader.resources;
 
 
 export class World2d {
 
-	public content = new Container();
+	public content = new ParticleContainer(15000, {
+		position: true,
+		rotation: true,
+		alpha: false,
+		scale: false,
+		uvs: false
+	});
 
 	private sobEvent: AppEvent = new AppEvent();// события
 	private cat: Sprite;
@@ -36,8 +43,8 @@ export class World2d {
 
 	public update() {
 		this.cat.rotation += 0.01;
-		this.cat.x += this.cat.vx;
-		this.cat.y += this.cat.vy;
+		this.cat.x += this.cat.vx || 0;
+		this.cat.y += this.cat.vy || 0;
 		if (this.rocket.x > 300) {
 			this.dimention = -1;
 			this.rocket.rotation = Math.PI;
@@ -50,15 +57,16 @@ export class World2d {
 
 
 	testcreate() {
+		let id = resources["resource/images/sprites.json"].textures;
 		//Create the `tileset` sprite from the texture
-		var texture = TextureCache["resource/images/tileset.png"];
-		//Create a rectangle object that defines the position and
-		//size of the sub-image you want to extract from the texture
-		var rectangle = new Rectangle(3 * 32, 2 * 32, 32, 32);
-		//Tell the texture to use that rectangular section
-		// texture.frame = rectangle; //Create the sprite from the texture
-		texture.frame = rectangle;
-		this.rocket = new Sprite(texture);
+		// var texture = TextureCache["resource/images/tileset.png"];
+		// //Create a rectangle object that defines the position and
+		// //size of the sub-image you want to extract from the texture
+		// var rectangle = new Rectangle(3 * 32, 2 * 32, 32, 32);
+		// //Tell the texture to use that rectangular section
+		// // texture.frame = rectangle; //Create the sprite from the texture
+		// texture.frame = rectangle;
+		this.rocket = new Sprite(id['6.png']);
 
 		//Position the rocket sprite on the canvas
 		this.rocket.position.set(100, 100);
@@ -66,14 +74,13 @@ export class World2d {
 		this.content.addChild(this.rocket);
 
 		//Create the `cat` sprite from the texture
-		this.cat = new Sprite(resources["resource/images/imgif.gif"].texture);
+		this.cat = new Sprite(id['5.png']);
 
 		this.cat.position.set(50, 50);
 		this.cat.scale.set(0.5, 0.5);
 		this.cat.anchor.set(0.5, 0.5);
 		this.content.addChild(this.cat);
 
-		let id = resources["resource/images/sprites.json"].textures;
 		console.log(resources, id);
 		let sprite = new Sprite(id['1.png']);
 		this.content.addChild(sprite);
